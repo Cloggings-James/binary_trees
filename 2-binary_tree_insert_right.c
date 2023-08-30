fe8778aed120c51516d6ac85e474cbda23379ad8
Cloggin-James <james.james@gmail.com>
2023-08-30 12:50:20 -0700

    binary_tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,3 @@
-#include <stddef.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -11,23 +9,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    if (!parent)
-        return (NULL);
+if (!parent)
+return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
+new_node = binary_tree_node(parent, value);
+if (!new_node)
+return (NULL);
 
-    if (parent->right)
-    {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-    }
+if (parent->right)
+{
+new_node->right = parent->right;
+parent->right->parent = new_node;
+}
 
-    parent->right = new_node;
+parent->right = new_node;
 
-    return (new_node);
+return (new_node);
 }
 
